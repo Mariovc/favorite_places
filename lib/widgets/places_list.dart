43e843bbde8658891e383b3fd2245560cef8e7a6
DESKTOP-0S94A87\Mario <mariovelasco3@hotmail.com>
2023-09-20 11:01:35 +0200

    Add detail page

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/model/place.dart';
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -21,6 +22,13 @@ class PlacesList extends StatelessWidget {
                 .titleMedium!
                 .copyWith(color: Theme.of(context).colorScheme.onBackground),
           ),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => PlaceDetailScreen(place: places[index]),
+              ),
+            );
+          },
         );
       },
     );
