c3c35cf93af2c8be3bc433e38eed71e148f9cffa
DESKTOP-0S94A87\Mario <mariovelasco3@hotmail.com>
2023-09-21 13:57:05 +0200

    Storing the picked image locally

--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -2,14 +2,20 @@ import 'dart:io';
 
 import 'package:favorite_places/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class PlacesNotifier extends StateNotifier<List<Place>> {
   PlacesNotifier() : super(const []);
 
-  void addPlace(String newPlace, File image, PlaceLocation location) {
+  void addPlace(String newPlace, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
     state = [
       ...state,
-      Place(title: newPlace, image: image, location: location),
+      Place(title: newPlace, image: copiedImage, location: location),
     ];
   }
 }
