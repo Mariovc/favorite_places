24eb4f71d7747ea68a8877df5e084d795317ce9d
DESKTOP-0S94A87\Mario <mariovelasco3@hotmail.com>
2023-09-22 09:27:30 +0200

    Storing place data in SQL Database and loading it

--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -4,19 +4,67 @@ import 'package:favorite_places/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    version: 1,
+  );
+  return db;
+}
 
 class PlacesNotifier extends StateNotifier<List<Place>> {
   PlacesNotifier() : super(const []);
 
-  void addPlace(String newPlace, File image, PlaceLocation location) async {
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              coordinates: PlaceCoordinates(
+                latitude: row['lat'] as double,
+                longitude: row['lng'] as double,
+              ),
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final filename = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$filename');
 
-    state = [
-      ...state,
-      Place(title: newPlace, image: copiedImage, location: location),
-    ];
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
+
+    final db = await _getDatabase();
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.coordinates.latitude,
+      'lng': newPlace.location.coordinates.longitude,
+      'address': newPlace.location.address,
+    });
+
+    state = [newPlace, ...state];
   }
 }
 
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -12,6 +12,14 @@ class PlacesScreen extends ConsumerStatefulWidget {
 }
 
 class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(placesProvider.notifier).loadPlaces();
+  }
+
   @override
   Widget build(BuildContext context) {
     final places = ref.watch(placesProvider);
@@ -42,8 +50,16 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
             )
           : Padding(
               padding: const EdgeInsets.all(8),
-              child: PlacesList(
-                places: places,
+              child: FutureBuilder(
+                future: _placesFuture,
+                builder: (context, snapshot) =>
+                    snapshot.connectionState == ConnectionState.waiting
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : PlacesList(
+                            places: places,
+                          ),
               ),
             ),
     );
